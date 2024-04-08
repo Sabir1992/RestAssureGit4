40ae71257f03af7608e329f26087cec1b90939be
Sabir1992 <Sabir_Tahirli@outlook.com>
2024-04-08 21:02:51 +0400

    2ci feature elave eledim, icin yazmaq lazimdir

---
+++ src/test/java/com/BookingProject/Steps/TestSteps.java
@@ -0,0 +1,31 @@
+package com.BookingProject.Steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TestSteps {
+
+
+    @Given("First we need create token for creating booking1")
+    public void first_we_need_create_token_for_creating_booking1() {
+
+    }
+    @Given("Create token1")
+    public void create_token1() {
+
+    }
+    @When("Create booking1")
+    public void create_booking1() {
+
+    }
+    @Then("Assertions checking1")
+    public void assertions_checking1() {
+
+    }
+    @Then("Delete booking1")
+    public void delete_booking1() {
+
+    }
+
+}
---
+++ target/test-classes/test.feature
@@ -0,0 +1,7 @@
+Feature: In this feature we can able create and delete booking1
+  Scenario: Positive booking create and delete it1
+    Given First we need create token for creating booking1
+    And Create token1
+    When Create booking1
+    Then Assertions checking1
+    And Delete booking1
